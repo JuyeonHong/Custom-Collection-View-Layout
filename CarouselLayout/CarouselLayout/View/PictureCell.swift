6d1e4c09e8feab7781a79ae3fed16db9b3bf6f9a
hongjuyeon <coke1317@naver.com>
2019-12-03 17:56:06 +0900

    Picture cell 추가, UIViewController -> UICollectionViewController로 변경

---
+++ CarouselLayout/CarouselLayout/View/PictureCell.swift
@@ -0,0 +1,16 @@
+//
+//  PictureCell.swift
+//  CarouselLayout
+//
+//  Created by hongjuyeon_dev on 2019/12/03.
+//  Copyright © 2019 hongjuyeon_dev. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
