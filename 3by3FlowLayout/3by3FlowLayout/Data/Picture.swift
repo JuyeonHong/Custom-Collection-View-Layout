89727c88f88033f0b5082fc059016c2d940b775f
hongjuyeon <coke1317@naver.com>
2019-12-03 14:51:30 +0900

    이미지 assets, plist 추가

---
+++ 3by3FlowLayout/3by3FlowLayout/Data/Picture.swift
@@ -0,0 +1,9 @@
+//
+//  Picture.swift
+//  3by3FlowLayout
+//
+//  Created by hongjuyeon_dev on 2019/12/03.
+//  Copyright © 2019 hongjuyeon_dev. All rights reserved.
+//
+
+import Foundation
---
+++ 3by3FlowLayout/3by3FlowLayout/View/PictureCell.swift
@@ -0,0 +1,13 @@
+//
+//  PictureCell.swift
+//  3by3FlowLayout
+//
+//  Created by hongjuyeon_dev on 2019/12/03.
+//  Copyright © 2019 hongjuyeon_dev. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+    
+}
