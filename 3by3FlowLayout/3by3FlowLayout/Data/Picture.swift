8388d29869ccde1bc8889ce481851690e618d583
hongjuyeon <coke1317@naver.com>
2019-12-03 14:51:56 +0900

    Picture class 추가

--- 3by3FlowLayout/3by3FlowLayout/Data/Picture.swift
+++ 3by3FlowLayout/3by3FlowLayout/Data/Picture.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2019 hongjuyeon_dev. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Picture {
+    let image: String
+    let date: String
+    
+    init(image: String, date: String) {
+        self.image = image
+        self.date = date
+    }
+    
+    convenience init(dictionary: NSDictionary) {
+        let image = dictionary["Image"] as? String
+        let date = dictionary["Date"] as? String
+        
+        self.init(image: image!, date: date!)
+    }
+    
+    static func loadPictures() -> [Picture] {
+        var pictures = [Picture]()
+        if let path = Bundle.main.path(forResource: "Pictures", ofType: "plist"), let picArray = NSArray(contentsOfFile: path) {
+            for item in picArray {
+                let picture = Picture(dictionary: item as? NSDictionary ?? [:])
+                pictures.append(picture)
+            }
+        }
+        return pictures
+    }
+}
