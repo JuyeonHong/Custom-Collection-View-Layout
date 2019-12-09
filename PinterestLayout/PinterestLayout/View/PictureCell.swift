a42ffc5aa42d3e30dcb6ee20fc04b25520efb5d0
hongjuyeon <coke1317@naver.com>
2019-12-09 09:43:58 +0900

    CollectionViewCell 추가(PictureCell) IB outlet 연결 및 데이터 받는 부분까지 직성

---
+++ PinterestLayout/PinterestLayout/View/PictureCell.swift
@@ -0,0 +1,25 @@
+//
+//  PictureCell.swift
+//  PinterestLayout
+//
+//  Created by hongjuyeon_dev on 2019/12/09.
+//  Copyright © 2019 hongjuyeon_dev. All rights reserved.
+//
+
+import UIKit
+
+class PictureCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var picture: Picture? {
+        didSet {
+            if let pic = picture {
+                imageView.image = UIImage(named: pic.image)
+                dateLabel.text = pic.date
+                descriptionLabel.text = pic.description
+            }
+        }
+    }
+}
