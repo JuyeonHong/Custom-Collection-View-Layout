e0b0fb80be6e5b090e82c3c35367c1da0bd1dede
hongjuyeon <coke1317@naver.com>
2019-12-09 13:28:46 +0900

    VC - collectionView 기본 데이터 뿌리기, 2 columns로 나오도록 초기 레이아웃 설정

--- PinterestLayout/PinterestLayout/ViewController.swift
+++ PinterestLayout/PinterestLayout/ViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     let pictures = Picture.loadPictures()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView?.contentInset = UIEdgeInsets(top: 23, left: 16, bottom: 10, right: 16)
     }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PictureCell", for: indexPath) as! PictureCell
+        cell.picture = pictures[indexPath.item]
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSize = (UIScreen.main.bounds.width - (collectionView.contentInset.left + collectionView.contentInset.right + 10)) / 2
+        return CGSize(width: itemSize, height: itemSize)
+    }
+}
